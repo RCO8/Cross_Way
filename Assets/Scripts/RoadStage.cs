ea8112ed088558b62db88fac139657beafb10bd3
RCO8 <gurwo63@naver.com>
2024-06-14 12:45:38 +0900

    Way1, Way2 생성로직 교체

--- Assets/Scripts/RoadStage.cs
+++ Assets/Scripts/RoadStage.cs
@@ -13,15 +13,16 @@ public class RoadStage : MonoBehaviour
     {
         carPrefab = Resources.Load<GameObject>("Prefabs/Car");
 
+        //WayLin2
+        carPrefab.GetComponent<Car>().SetDirection(false);
+        Instantiate(carPrefab, WayLine2.transform.position + new Vector3(10, 1.25f), Quaternion.identity);
+        Instantiate(carPrefab, WayLine2.transform.position + new Vector3(10, -0.4f), Quaternion.identity);
+
         //WayLine1
         carPrefab.GetComponent<Car>().SetDirection(true);
         Instantiate(carPrefab, WayLine1.transform.position + new Vector3(-10, 1.25f), Quaternion.identity);
         Instantiate(carPrefab, WayLine1.transform.position + new Vector3(-10, -0.4f), Quaternion.identity);
 
-        //WayLin2
-        carPrefab.GetComponent<Car>().SetDirection(false);
-        Instantiate(carPrefab, WayLine2.transform.position + new Vector3(10, 1.25f), Quaternion.identity);
-        Instantiate(carPrefab, WayLine2.transform.position + new Vector3(10, -0.4f), Quaternion.identity);
     }
 
     void Update()
