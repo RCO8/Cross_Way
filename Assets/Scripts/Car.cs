562494403407bfdc421e3042c9d228b4099ccbfb
RCO8 <gurwo63@naver.com>
2024-06-14 11:40:23 +0900

    자동차 상호작용 로직 구현 & 스프라이트 정렬

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float CarSpeed = 10f;
+
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rgdBody2D;
+
+    private Vector2 moveDirection = Vector2.zero;
+
+    private void Awake()
     {
-        
+        rgdBody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if(spriteRenderer.flipX)    //우로 전진
+            moveDirection = Vector2.right;
+        else    //좌로 전진
+            moveDirection = Vector2.left;
+    }
+
+    private void FixedUpdate()
+    {
+        rgdBody2D.velocity = moveDirection * CarSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("게임 오버");
+        }
     }
 }
