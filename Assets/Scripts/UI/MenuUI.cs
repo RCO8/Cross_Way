b8e8c90edac4dee0178fe5bfc9c1a77ff2314e95
RCO8 <gurwo63@naver.com>
2024-06-14 19:13:15 +0900

    메뉴 UI 구상

---
+++ Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuUI : MonoBehaviour
+{
+    public void OnMenuEnter(InputAction.CallbackContext context)
+    {
+        Debug.Log("¸Þ´º");
+    }
+}
