b3f14902d3003d1e12f3d6281dfe7459a199afee
RCO8 <gurwo63@naver.com>
2024-06-16 16:30:21 +0900

    Resume버튼 기능 작업

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -3,18 +3,48 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI CaptionText;
+    [SerializeField]
+    private TextMeshProUGUI CaptionText;
+    [SerializeField]
+    private TextMeshProUGUI ResumeButtonPanel;
 
-    private void Start()
-    {
-
-    }
+    private string GameOverText = "GAME OVER";
 
     public void GameOverPanel()
     {
-        CaptionText.text = "Game Over";
+        CaptionText.text = GameOverText;
+        ResumeButtonText();
+    }
+
+    private void ResumeButtonText()
+    {
+        if (CaptionText.text.Equals(GameOverText))
+            ResumeButtonPanel.text = "RESTART";
+        else
+            ResumeButtonPanel.text = "RESUME";
+    }
+
+    public void ResumeButtonAction()    //버튼이 눌려졌을때
+    {
+        if (CaptionText.text.Equals(GameOverText))
+        {
+            //다시 시작
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            //계속
+            GameManager.Instance.OpenMenu(false);
+        }
+    }
+
+    public void ExitButtonAction()
+    {
+        //게임 종료
     }
 }
